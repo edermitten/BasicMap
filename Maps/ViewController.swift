071d3b226846f50cd0b658e01b951f117e377aa6
Eder <edermitten@gmail.com>
2018-09-20 14:52:13 -0500

    second commit

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -23,10 +23,36 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let lonDelta: CLLocationDegrees = 0.05
         
         let spam : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
-        let location : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let region : MKCoordinateRegion = MKCoordinateRegion(center: location, span: spam)
+        let coordinates : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region : MKCoordinateRegion = MKCoordinateRegion(center: coordinates, span: spam)
         
         map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "Magic Kingdom"
+        annotation.subtitle = "Best place ever.."
+        annotation.coordinate = coordinates
+        
+        map.addAnnotation(annotation)
+        
+        //UI Long Press Gesture Recognizer
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        uilpgr.minimumPressDuration = 2
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    @objc func longpress(gestureRecognizer: UIGestureRecognizer) {
+        
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "New place"
+        annotation.subtitle = "Maybe I'll go here...."
+        map.addAnnotation(annotation)
+        
+        
     }
 
 
